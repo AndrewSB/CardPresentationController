7307851eb942ac06fb5d6bb584b66d04ba2b6099
John Bolliger <johnbolliger@gmail.com>
2020-10-03 22:53:35 -0600

    Add package swift manifest

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(name: "CardPresentationController",
+                      platforms: [.macOS(.v10_12),
+                                  .iOS(.v10),
+                                  .tvOS(.v10),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "CardPresentationController",
+                                          targets: ["CardPresentationController"])],
+                      targets: [.target(name: "CardPresentationController",
+                                        path: "Source",
+                                        linkerSettings: [.linkedFramework("UIKit",
+                                                                          .when(platforms: [.iOS,
+                                                                                            .macOS,
+                                                                                            .tvOS,
+                                                                                            .watchOS]))]),
+                                ],
+                      swiftLanguageVersions: [.v5])
